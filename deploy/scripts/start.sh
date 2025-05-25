d6a99dd1a8ef7884b19b997b45e04a56149e7e3f
Quoc Truong LE <lqtruong@gmail.com>
2025-05-25 22:51:28 +0800

    Appspec start

--- deploy/scripts/start.sh
+++ deploy/scripts/start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 echo "Starting application..."
-cd /home/ec2-user/app
-ls /home/ec2-user/app
-# nohup java -jar /home/ec2-user/app/app.jar --spring.config.location=/home/ec2-user/app/application.properties > /home/ec2-user/app/app.log 2>&1 &
+nohup java -jar /home/ec2-user/app/app.jar --spring.config.location=/home/ec2-user/app/application.properties --server.port=8080 > /dev/null 2>&1 &
