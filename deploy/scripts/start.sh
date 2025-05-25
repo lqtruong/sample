97a490d18ebd60ed755d30358afc0c17047451a8
Quoc Truong LE <lqtruong@gmail.com>
2025-05-25 20:19:54 +0800

    Disable start to use userData

--- deploy/scripts/start.sh
+++ deploy/scripts/start.sh
@@ -2,4 +2,4 @@
 echo "Starting application..."
 cd /home/ec2-user/app
 ls /home/ec2-user/app
-nohup java -jar /home/ec2-user/app/app.jar --spring.config.location=/home/ec2-user/app/application.properties > /home/ec2-user/app/app.log 2>&1 &
+# nohup java -jar /home/ec2-user/app/app.jar --spring.config.location=/home/ec2-user/app/application.properties > /home/ec2-user/app/app.log 2>&1 &
